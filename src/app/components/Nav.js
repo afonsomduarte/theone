9a2c5676475abbb07fb0930e172a3e81e28ea947
Afonso <afonso.martins.duarte@gmail.com>
2018-08-11 17:36:31 +0100

    é desta

--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -1,5 +1,5 @@
 // Nav.js
-import React from '../../../../../Library/Caches/typescript/2.9/node_modules/@types/react'
+import React from 'react'
 import styled, { css } from 'styled-components'
 
 const Header = styled.header`
